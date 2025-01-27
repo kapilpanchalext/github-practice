040ea5c43cfef652d9c6973e250ccff278fcac6f
kapil.panchal.ext@nmims.edu <kapil.panchal.ext@nmims.edu>
2025-01-27 17:56:23 +0530

    [refactor] newImage Logic

--- git_practice_1/src/main/java/com/java/git/App.java
+++ git_practice_1/src/main/java/com/java/git/App.java
@@ -2,8 +2,15 @@ package com.java.git;
 
 public class App {
     public static void main(String[] args) {
+<<<<<<< Upstream, based on branch 'main' of ssh://git@github.com/kapilpanchalext/github-practice.git
         System.out.println("BugFix");
         
         System.out.println("Main");
+=======
+        System.out.println("Github Commit 1");
+        System.out.println("Github Commit 2");
+        System.err.println("Git Branch - newImage");
+        System.err.println("Git newImage - Logic Changed");
+>>>>>>> f8f192b [refactor] newImage Logic
     }
 }
