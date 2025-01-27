27323b1554c7d8b06f77961f2324c3776c77f561
kapil.panchal.ext@nmims.edu <kapil.panchal.ext@nmims.edu>
2025-01-27 17:31:41 +0530

    [branch] newImage Branch

--- git_practice_1/src/main/java/com/java/git/App.java
+++ git_practice_1/src/main/java/com/java/git/App.java
@@ -4,9 +4,16 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Github Commit 1");
         System.out.println("Github Commit 2");
+<<<<<<< Upstream, based on branch 'main' of ssh://git@github.com/kapilpanchalext/github-practice.git
         System.err.println("Git Branch - newImage");
+=======
+<<<<<<< Upstream, based on origin/bugFix
+>>>>>>> 8491042 [branch] newImage Branch
         System.err.println("Github Branch - bugFix");
         System.err.println("Created one more bugfix");
         System.err.println("Rebase BugFix to Main");
+=======
+        System.err.println("Git Branch - newImage");
+>>>>>>> 959ed2c [branch] newImage Branch
     }
 }
