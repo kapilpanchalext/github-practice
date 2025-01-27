364b6104e70fa2e783bb63393b29366758c391d9
kapil.panchal.ext@nmims.edu <kapil.panchal.ext@nmims.edu>
2025-01-27 17:49:26 +0530

    [bugfix] added logic

--- git_practice_1/src/main/java/com/java/git/App.java
+++ git_practice_1/src/main/java/com/java/git/App.java
@@ -5,8 +5,13 @@ public class App {
         System.out.println("Github Commit 1");
         System.out.println("Github Commit 2");
         System.err.println("Git Branch - newImage");
+<<<<<<< Upstream, based on branch 'newImage' of ssh://git@github.com/kapilpanchalext/github-practice.git
         System.err.println("Git newImage - Logic Changed");
         
         System.err.println("Rebase - newImage");
+=======
+        System.err.println("Github Branch - bugFix");
+        System.err.println("Created one more bugfix");
+>>>>>>> 946f5a7 [bugfix] added logic
     }
 }
