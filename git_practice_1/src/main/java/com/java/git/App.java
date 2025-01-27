2765ada9ec73c2d60295afe40a89f3fe22e14ad4
kapil.panchal.ext@nmims.edu <kapil.panchal.ext@nmims.edu>
2025-01-27 17:24:27 +0530

    [logic] added logic

--- git_practice_1/src/main/java/com/java/git/App.java
+++ git_practice_1/src/main/java/com/java/git/App.java
@@ -4,5 +4,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Github Commit 1");
         System.out.println("Github Commit 2");
+        System.err.println("Git Branch 1");
     }
 }
