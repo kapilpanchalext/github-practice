e4a0ae18657ab04f0f141dc6229e02f33f518ec6
kapil.panchal.ext@nmims.edu <kapil.panchal.ext@nmims.edu>
2025-01-28 17:53:22 +0530

    [rebase] main - newImage

--- git_practice_1/src/main/java/com/java/git/App.java
+++ git_practice_1/src/main/java/com/java/git/App.java
@@ -4,5 +4,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("NewImage");
         System.out.println("Main");
+        System.out.println("NewImage");
     }
 }
