13e9d403bed7c42cb9c87c5826db2eb13002c6ec
kapil.panchal.ext@nmims.edu <kapil.panchal.ext@nmims.edu>
2025-01-28 17:46:32 +0530

    [bugfix]

--- git_practice_1/src/main/java/com/java/git/App.java
+++ git_practice_1/src/main/java/com/java/git/App.java
@@ -2,14 +2,7 @@ package com.java.git;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Github Commit 1");
-        System.out.println("Github Commit 2");
-        System.err.println("Github Branch - bugFix");
-        System.err.println("Created one more bugfix");
-        System.err.println("Rebase BugFix to Main");
-        System.err.println("Git Branch 1");
-        System.err.println("Git Branch - newImage");
+        System.out.println("BugFix");
         
-        System.err.println("BugFix Rebase 2");
     }
 }
