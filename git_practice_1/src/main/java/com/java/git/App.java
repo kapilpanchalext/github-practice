b613c0dd3bdc7cc449ca0bb50b51a7196972431e
kapil.panchal.ext@nmims.edu <kapil.panchal.ext@nmims.edu>
2025-01-28 17:44:45 +0530

    [refactor]

--- git_practice_1/src/main/java/com/java/git/App.java
+++ git_practice_1/src/main/java/com/java/git/App.java
@@ -2,15 +2,6 @@ package com.java.git;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Github Commit 1");
-        System.out.println("Github Commit 2");
-        System.err.println("Git Branch - newImage");
-        System.err.println("Github Branch - bugFix");
-        System.err.println("Created one more bugfix");
-        System.err.println("Rebase BugFix to Main");
-        System.err.println("Git Branch - newImage");
-        System.err.println("Git newImage - Logic Changed");
-        
-        System.err.println("Rebase - newImage");
+        System.out.println("Main");
     }
 }
