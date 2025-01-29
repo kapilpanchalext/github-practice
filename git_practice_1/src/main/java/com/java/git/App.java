f1e7b458a3a023e3410f5244bb73c6581c95aece
kapil.panchal.ext@nmims.edu <kapil.panchal.ext@nmims.edu>
2025-01-29 17:19:12 +0530

    [rebase]

--- git_practice_1/src/main/java/com/java/git/App.java
+++ git_practice_1/src/main/java/com/java/git/App.java
@@ -2,13 +2,7 @@ package com.java.git;
 
 public class App {
     public static void main(String[] args) {
-<<<<<<< Upstream, based on branch 'newImage' of ssh://git@github.com/kapilpanchalext/github-practice.git
         System.out.println("Main");
         System.out.println("NewImage");
-=======
-        System.out.println("Github Commit 1");
-        System.out.println("Github Commit 2");
-        System.err.println("Github Branch - bugFix");
->>>>>>> 5c873de [branch] bugFix added
     }
 }
