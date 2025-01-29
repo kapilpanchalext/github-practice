7c525214d3a6db0d4ee1073cc8c59872da3fb706
kapil.panchal.ext@nmims.edu <kapil.panchal.ext@nmims.edu>
2025-01-29 17:52:42 +0530

    [refactor]

--- git_practice_1/src/main/java/com/java/git/App.java
+++ git_practice_1/src/main/java/com/java/git/App.java
@@ -3,7 +3,5 @@ package com.java.git;
 public class App {
     public static void main(String[] args) {
         System.out.println("NewImage");
-        System.out.println("Main");
-        System.out.println("NewImage");
     }
 }
