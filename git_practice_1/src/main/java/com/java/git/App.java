9d669ffb3d706e51219a4c029d8036912c5a0faa
kapil.panchal.ext@nmims.edu <kapil.panchal.ext@nmims.edu>
2025-01-30 17:51:47 +0530

    [logic] added

--- git_practice_1/src/main/java/com/java/git/App.java
+++ git_practice_1/src/main/java/com/java/git/App.java
@@ -3,6 +3,7 @@ package com.java.git;
 public class App {
     public static void main(String[] args) {
         System.out.println("BugFix");
+        System.err.println("Added BugFix - Logic1");
         
     }
 }
